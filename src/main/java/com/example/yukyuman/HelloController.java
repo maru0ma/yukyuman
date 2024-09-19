189897e32cddba98d0fc2d5b28c76c5c973c4cd8
maru <144039799+maru0ma@users.noreply.github.com>
2024-09-19 22:00:32 +0900

    TOPページの処理を追加

--- src/main/java/com/example/yukyuman/HelloController.java
+++ src/main/java/com/example/yukyuman/HelloController.java
@@ -1,17 +1,30 @@
 package com.example.yukyuman;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class HelloController {
     
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("message", "hello momonga!");
-        return "index";
+    public ModelAndView index(ModelAndView mav) {
+        mav.setViewName("index");
+        mav.addObject("restDays",5);
+        mav.addObject("exactlyGetDays",2);
+        mav.addObject("disappearanceDays",1);
+ 
+        return mav;
     }
+
+    @GetMapping("/entry")
+    public ModelAndView entry(ModelAndView mav) {
+        mav.setViewName("entry");
+ 
+        return mav;
+    } 
+
+
     
 }
